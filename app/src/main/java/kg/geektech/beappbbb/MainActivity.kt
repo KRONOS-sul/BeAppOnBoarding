acb4b78d69c2e980b2a2104d2aa4119ea6af9043
Sultan <93419011+KRONOS-sul@users.noreply.github.com>
2022-07-16 15:42:43 +0300

    сделал первый экран и обновил все зависимости до последней версии

---
+++ app/src/main/java/kg/geektech/beappbbb/MainActivity.kt
@@ -0,0 +1,46 @@
+package kg.geektech.beappbbb
+
+import android.view.LayoutInflater
+import android.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kg.geektech.beappbbb.base.BaseActivity
+import kg.geektech.beappbbb.base.BaseViewModel
+import kg.geektech.beappbbb.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
+
+    override val viewModel: BaseViewModel by lazy {
+        ViewModelProvider(this)[BaseViewModel::class.java]
+    }
+
+    override fun inflateViewBinding(inflater: LayoutInflater): ActivityMainBinding {
+        return ActivityMainBinding.inflate(inflater)
+    }
+
+    override fun initView() {
+        //инициализация actionBar
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        //инициализация bottom navigation
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.recordsFragment, R.id.salonsFragment, R.id.accountsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+    }
+}
---
+++ app/src/main/java/kg/geektech/beappbbb/ui/screens/OnBoarding.kt
@@ -0,0 +1,19 @@
+package kg.geektech.beappbbb.ui.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kg.geektech.beappbbb.R
+
+class OnBoarding : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+    }
+}
